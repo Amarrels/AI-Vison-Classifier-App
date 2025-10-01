1628ee5ebdb20091179deb003249e639983b5b56
Amarra Houraney <ahouraney2021@fau.edu>
2025-09-30 21:47:23 -0400

    Initial Commit

---
+++ Vision Classifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vision Classifier
+//
+//  Created by Amarra Houraney on 9/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vision Classifier/Vision_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_ClassifierApp.swift
+//  Vision Classifier
+//
+//  Created by Amarra Houraney on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Vision_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
